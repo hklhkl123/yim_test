f435ca3aaa33a366cb7ee195a415f0d947658d30
hklhkl123 <hklhkl123@sina.com>
2021-05-26 18:25:25 +0800

    let us go

---
+++ scripts/test_debug.py
@@ -0,0 +1,29 @@
+import time
+import os,sys
+import pytest
+import allure
+
+sys.path.append(os.getcwd())
+
+from base.base_driver import init_driver
+from page.message_page import MessagePage
+from base.read_yaml import Read_Data
+
+class TestDebug(object):
+    def setup(self):
+        self.driver = init_driver()
+        self.message_page = MessagePage(self.driver)
+
+    def test_debug_1(self):
+        assert True
+
+    def test_debug_2(self):
+        assert False
+
+    def teardown(self):
+        self.driver.quit()
+
+
+
+if __name__ == "__main__":
+    pytest.main(["-s","test_debug.py"])
