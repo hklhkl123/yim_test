986195420a327a4aea177d47b663de6f1df035ca
panbihong <1527276579@qq.com>
2021-06-17 16:47:03 +0800

    add:增加app第一期33个接口的测试用例

---
+++ app_api_test/AppCase/allApiTestRun.py
@@ -0,0 +1,31 @@
+import time
+import unittest
+import HTMLTestRunner
+from app_api_test.common.setting import filename1, case_path1, report_name
+
+# 获取所有测试用例
+def get_allcase():
+    # 测试用例存放路径
+    case_path = case_path1
+    # 获取路径下面所有包含test_*.py命名的测试用例（包括子文件下的测试用例）
+    discover_all_cases = unittest.defaultTestLoader.discover(case_path, pattern="test_*.py", top_level_dir=None)
+    # 把测试用例加进测试用例集合suite
+    suite = unittest.TestSuite()
+    suite.addTest(discover_all_cases)
+    # 返回用例集合suite
+    return suite
+
+
+if __name__ == '__main__':
+    # 运行测试用例
+    timestr = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))  # 本地日期时间作为测试报告的名字
+    filename = filename1 + report_name + timestr + '.html'  # 这个路径改成自己的目录路径
+    fp = open(filename, 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+        stream=fp,
+        title='测试结果',
+        description='Report_description'
+    )
+    # 执行测试用例，调用get_allcase()方法会返回测试用例集合给run()方法运行
+    runner.run(get_allcase())
+    fp.close()
---
+++ app_api_test/common/login1.py
@@ -0,0 +1,79 @@
+import jsonpath
+import requests
+import hashlib
+
+# 这里是接口地址，必填
+from app_api_test.common.setting import baseurl
+
+url1 = baseurl
+
+
+# app登录接口获取token
+def test_login_code():
+    # 请求头
+    headers = {
+
+    }
+    # 请求体
+
+    payload = {
+        "phone": "18820787573",
+        "area_code": "+86",
+        "code": "1234",
+        "sign": "7f6d0ffc-b0e2-495e-b438-6ac0a7f4155a",
+        "app_version": "1.0",
+
+    }
+    # 请求地址
+    url = url1 + '/user/login/code'
+    # 发起请求
+    response = requests.request("POST", url, headers=headers, params=payload)
+    # 接口返结果处理
+    result = response.json()
+    # print(result)
+    # print(result["code"])
+    # print(result["message"])
+
+    # 获取token
+    token1 = jsonpath.jsonpath(result, '$..token')
+    # 格式化token
+    token2 = "".join(token1)
+    # 返回token
+    print(token2)
+    return token2
+
+
+# 云聊登录接口获取token
+def getToken():
+    payload = {'account': '张英波',
+               'password': 'test888888',
+               'sign': 'f9a69c19-25c7-4ebf-ae96-468f80c487db',
+               'app_version': '1.17.3'
+               }
+
+    url = url1 + '/staff/signin'
+    response = requests.request("POST", url, data=payload)
+    result = response.json()
+    # 获取token
+    token1 = jsonpath.jsonpath(result, '$..token')
+    # 格式化token
+    token2 = "".join(token1)
+    # 返回token
+    return token2
+
+
+# MD5加密，32位小写
+def md5vale(key):
+    input_name = hashlib.md5()
+
+    input_name.update(key.encode("utf-8"))
+
+    sign = input_name.hexdigest()
+
+    print(key, "  ---->  ", sign)
+    # 返回加密后的sign
+    return sign
+
+
+if __name__ == '__main__':
+    print(getToken())
